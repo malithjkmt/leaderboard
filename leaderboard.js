0bbd1f08883e3e8f6db6ce6175856bae5e65cf1d
Malith <malit.tilak@gmail.com>
2016-03-12 01:53:42 +0530

    secure app by moving some methods from client to server

--- leaderboard.js
+++ leaderboard.js
@@ -5,6 +5,26 @@ if (Meteor.isServer){
         var currentUserId = this.userId;
         return PlayersList.find({createdBy:currentUserId})
     });
+
+
+    Meteor.methods({
+        'insertPlayerData': function(playerNameVar){
+            var currentUserId = Meteor.userId();
+            PlayersList.insert({
+                name: playerNameVar,
+                score: 0,
+                createdBy: currentUserId
+            });
+        },
+        'removePlayer': function(selectedPlayer){
+            var currentUserId = Meteor.userId();
+            PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+        },
+        'updateScore': function(selectedPlayer, scoreValue){
+            var currentUserId = Meteor.userId();
+            PlayersList.update({_id: selectedPlayer, createdBy: currentUserId}, {$inc: {score: scoreValue} });
+        }
+    });
 }
 
 
@@ -13,7 +33,6 @@ if (Meteor.isClient){
 
     Template.leaderboard.helpers({
         'player': function(){
-            var currentUserId = Meteor.userId();
             return PlayersList.find({}, {sort: {score:-1, name:1}})
         },
         'selectedClass': function(){
@@ -36,18 +55,17 @@ if (Meteor.isClient){
         },
         'click .increment': function() {
             var selectedPlayer = Session.get('selectedPlayer');
-            PlayersList.update(selectedPlayer, {$inc: {score: 5}});
+            Meteor.call('updateScore',selectedPlayer, 5);
         },
         'click .decrement': function(){
             var selectedPlayer = Session.get('selectedPlayer');
-            PlayersList.update(selectedPlayer, {$inc:{score: -5}} );
+            Meteor.call('updateScore',selectedPlayer, -5);
         },
         'click .RemovePlayer': function() {
             var r = confirm("Are you sure?");
             if (r == true) {
                 var selectedPlayer = Session.get('selectedPlayer');
-                console.log(selectedPlayer);
-                PlayersList.remove(selectedPlayer);
+                Meteor.call('removePlayer', selectedPlayer);
             }
         }
     });
@@ -58,14 +76,8 @@ if (Meteor.isClient){
             // Prevent default browser form submit
             event.preventDefault();
             var playerNameVar = event.target.playerName.value;
-
-            var currentUserId = Meteor.userId();
-            PlayersList.insert({
-                name: playerNameVar,
-                score: 0,
-                createdBy: currentUserId
-            });
             event.target.playerName.value = "";
+            Meteor.call('insertPlayerData', playerNameVar);
         }
     });
 
