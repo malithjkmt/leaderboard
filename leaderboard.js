f2647b8264004a66a0d0d7fc9eb307d3c6a09cac
Malith <malit.tilak@gmail.com>
2016-03-11 18:22:55 +0530

    used a session

--- leaderboard.js
+++ leaderboard.js
@@ -6,8 +6,20 @@ if (Meteor.isClient){
         'player': function(){
             return PlayersList.find()
         },
-        'totalPlayers': function(){
-            return PlayersList.find().count()
+        'selectedClass': function(){
+            var playerId = this._id;
+            var selectedPlayer = Session.get('selectedPlayer');
+            if(playerId == selectedPlayer){
+                return "selected"
+            }
         }
     });
+
+    Template.leaderboard.events({
+        'click .player': function() {
+            var playerId = this._id;  // get the unique id of clicked li element
+            Session.set('selectedPlayer', playerId);   // store it in the current session
+        }
+    });
+
 }
