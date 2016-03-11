5c5b5f3f0af57de62923eb8cbbf63af9e6c1581a
Malith <malit.tilak@gmail.com>
2016-03-11 07:12:59 +0530

    connect js to html and make the Leaderboard dynamic

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,13 @@
 PlayersList =  new Mongo.Collection('players');
+
+
+if (Meteor.isClient){
+    Template.leaderboard.helpers({
+        'player': function(){
+            return PlayersList.find()
+        },
+        'totalPlayers': function(){
+            return PlayersList.find().count()
+        }
+    });
+}
