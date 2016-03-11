1888dbebae3a3cac4e779b224a50e7aac5c8c12d
Malith <malit.tilak@gmail.com>
2016-03-11 20:52:58 +0530

    started using forms

--- leaderboard.js
+++ leaderboard.js
@@ -4,7 +4,7 @@ PlayersList =  new Mongo.Collection('players');
 if (Meteor.isClient){
     Template.leaderboard.helpers({
         'player': function(){
-            return PlayersList.find()
+            return PlayersList.find({}, {sort: {score:-1, name:1}})
         },
         'selectedClass': function(){
             var playerId = this._id;
@@ -12,6 +12,10 @@ if (Meteor.isClient){
             if(playerId == selectedPlayer){
                 return "selected"
             }
+        },
+        'showSelectedPlayer': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            return PlayersList.findOne(selectedPlayer)
         }
     });
 
@@ -19,6 +23,14 @@ if (Meteor.isClient){
         'click .player': function() {
             var playerId = this._id;  // get the unique id of clicked li element
             Session.set('selectedPlayer', playerId);   // store it in the current session
+        },
+        'click .increment': function() {
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.update(selectedPlayer, {$inc: {score: 5}});
+        },
+        'click .decrement': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.update(selectedPlayer, {$inc:{score: -5}} );
         }
     });
 
